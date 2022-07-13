8cc862a0a59ae74e625bddf6800abfd03c52b1f6
Dhohir Pradana <dhohirkudus@gmail.com>
2022-07-13 18:56:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  devServer: {
+    disableHostCheck: true
+  },
   plugins: [vue()],
   resolve: {
     alias: {
